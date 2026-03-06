be09948798c55fce0d5be82d832feaa6a4bf07b3
Aswin1-code <500aswin@gmail.com>
2026-03-06 20:32:45 +0530

    done

--- c/fact_recursive_func.c
+++ c/fact_recursive_func.c
@@ -6,14 +6,13 @@ int main()
    printf("Enter the number for finding its factorial :");
    scanf("%d",&n);
    f=factorial(n);
-   printf("\nFactorial of %d is %d",n,f);
+   printf("Factorial of %d is %d",n,f);
 }
 int factorial(int n)
 {
    int temp;
    if(n==0)
       return 1;
-   else
-      temp=n*factorial(n-1);
-   return temp;
+   return n*factorial(n-1);
+
 }
